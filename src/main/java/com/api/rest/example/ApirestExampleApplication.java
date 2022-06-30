34989ca3d1f9046b5f095f08c6f43fd48d85c3f3
Oscar Ruiz <blackbird3871@gmail.com>
2022-06-30 16:28:24 -0500

    API REST (get, post, put, delete) sin base de datos (con java faker) y sin lombook

---
+++ src/main/java/com/api/rest/example/ApirestExampleApplication.java
@@ -0,0 +1,13 @@
+package com.api.rest.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestExampleApplication.class, args);
+	}
+
+}
